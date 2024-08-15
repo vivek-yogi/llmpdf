30b5b4bb91e5e11089d902874f909370da37e7f9
vivek-yogi <101085320+vivek-yogi@users.noreply.github.com>
2024-08-15 23:49:17 +0530

    Project template added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup (
+    name = 'PDF Based LLM',
+    version = '0.0.0',
+    author = 'vivek yogi',
+    author_email='vivek87yogi@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
